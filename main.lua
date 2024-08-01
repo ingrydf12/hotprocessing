7f31d385d8988ad89c41d1b8cba074a6544a31d0
Renan Araujo Polainas <re.araujo.po@gmail.com>
2024-07-31 21:44:21 -0300

    movimentação melhor e mais tiros

--- main.lua
+++ main.lua
@@ -6,7 +6,11 @@ local green = {0, 1, 0}
 local pele = {1, 0.8, 0.6}
 
 local player = {}
-local tiro = {x = -20, y = -20, velx = 0, vely = 0}
+local tiros = {}
+
+local tiro_atual = 1
+LIMITE = 5
+
 
 -- MARK: - Load LOVE function
 function love.load()
@@ -17,21 +21,33 @@ function love.load()
     player.hand1 = {x = -40, y = -10, w = 10, h = 10}
     player.hand2 = {x = 30, y = -10, w = 10, h = 10}
     player.shirt = {x = -40, y = -5, w = 80, h = 15}
+
+    for i = 1, LIMITE do
+        tiros[i] = {x = -20, y = -20, velx = 0, vely = 0}
+    end
 end
 
 -- MARK: - Mov
 function love.update(dt)
+    local dir = {0, 0}
     if love.keyboard.isDown("a") then
-        posx = posx - spd
-    elseif love.keyboard.isDown("d") then
-        posx = posx + spd
-    elseif love.keyboard.isDown("w") then
-        posy = posy - spd
-    elseif love.keyboard.isDown("s") then
-        posy = posy + spd
+        dir[1] = -1
+    end
+    if love.keyboard.isDown("d") then
+        dir[1] = dir[1] + 1
+    end
+    if love.keyboard.isDown("w") then
+        dir[2] = -1
+    end
+    if love.keyboard.isDown("s") then
+        dir[2] = dir[2] + 1
     end
 
-    updateTiro()
+    posx, posy = posx + dir[1]*spd, posy + dir[2]*spd
+
+    for i = 1, LIMITE do
+        updateTiro(tiros[i])
+    end
 end
 
 -- MARK: - Draw
@@ -66,30 +82,38 @@ function love.draw()
     
     -- Draw tiro
     love.graphics.setColor(red)
-    love.graphics.circle("fill", tiro.x, tiro.y, 10)
+    for i = 1, LIMITE do
+        love.graphics.circle("fill", tiros[i].x, tiros[i].y, 10)
+    end
 end
 
 -- MARK: - Powpow setting
 function love.mousepressed(x, y, button, istouch, presses)
     if button == 1 then
+        
+        tiro_atual = tiro_atual + 1
+        if tiro_atual == LIMITE+1 then
+            tiro_atual = 1
+        end
+
         local angle = math.atan2(y - posy, x - posx)
-        tiro.x = posx
-        tiro.y = posy
-        tiro.velx = spd * 2 * math.cos(angle)
-        tiro.vely = spd * 2 * math.sin(angle)
+        tiros[tiro_atual].x = posx
+        tiros[tiro_atual].y = posy
+        tiros[tiro_atual].velx = spd * 2 * math.cos(angle)
+        tiros[tiro_atual].vely = spd * 2 * math.sin(angle)
     end
 end
 
-function updateTiro()
+function updateTiro(tiro)
     tiro.x = tiro.x + tiro.velx
     tiro.y = tiro.y + tiro.vely
     
     if tiro.x >= 800 or tiro.y >= 800 or tiro.x < 0 or tiro.y < 0 then
-        resetTiro()
+        resetTiro(tiro)
     end
 end
 
-function resetTiro()
+function resetTiro(tiro)
     tiro.x = -20
     tiro.y = -20
     tiro.velx = 0
